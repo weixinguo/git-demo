b9add0a2fae912a79f0d3a6be4ed52d696d18e47
weixin_global <315759265_global@qq.com>
2020-01-06 20:35:06 +0800

    111

--- src/main/java/com/demo/GitDemoApplication.java
+++ src/main/java/com/demo/GitDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoApplication {
+    //测试 哈哈
 
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
